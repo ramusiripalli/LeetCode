756408607b36aff44e7ec1d383035247e8276b73
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-07 12:34:05 +0530

    Time: 0 ms (100%), Space: 53.5 MB (78.82%) - LeetHub

--- 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.js
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.js
@@ -5,14 +5,14 @@
 var balancedStringSplit = function(s) {
     let temp = 0;
     let count = 0;
-    for(let i=0;i<s.length;i++){
-        if(s[i] === "R"){
+    for(let i=0;i<s.length;i++)
+    {
+        if(s[i] === 'R'){
             ++temp;
         }
         else{
             --temp;
         }
-
         if(temp === 0){
             ++count;
         }
