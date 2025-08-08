3d93a4203f6d091bae4a25e151ae040ed9256cea
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-08 06:58:01 +0530

    Time: 2 ms (45.18%), Space: 57.9 MB (56.15%) - LeetHub

--- 3194-find-words-containing-character/3194-find-words-containing-character.js
+++ 3194-find-words-containing-character/3194-find-words-containing-character.js
@@ -4,16 +4,15 @@
  * @return {number[]}
  */
 var findWordsContaining = function(words, x) {
-    const res = [];
-   for(let i=0;i<words.length;i++)
-   {
-    for(let j=0;j<words[i].length;j++){
-        if(words[i][j] === x){
-            res.push(i);
-            break;
+    let res = [];
+    for(let i=0;i<words.length;i++)
+    {
+        for(let j=0;j<words[i].length;j++){
+            if(words[i][j] === x){
+                res.push(i);
+                break;
+            }
         }
     }
-
-    }
-   return res;
+    return res;
 };
