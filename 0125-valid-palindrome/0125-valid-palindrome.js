2cde042389df23d5df1a0f3922aa99d252d178d8
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-08 11:58:34 +0530

    Time: 7 ms (38.46%), Space: 59.8 MB (33.06%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,16 +4,24 @@
  */
 var isPalindrome = function(s) {
     s = s.toLowerCase();
-    let filteredString = "";
-    let reversedString = "";
-    for(let i=0;i<s.length;i++){
-        if((s[i].charCodeAt() >= 'a'.charCodeAt() && s[i].charCodeAt() <= 'z'.charCodeAt()) 
-        || 
-        (s[i].charCodeAt() >= '0'.charCodeAt() && s[i].charCodeAt() <= '9'.charCodeAt())
-         ){
-            filteredString = filteredString + s[i];
-            reversedString = s[i] + reversedString;
+    let i = 0;
+    let j = s.length-1;
+    while(i<j){
+        if(!s[i].match(/[a-z0-9]/i)){
+            ++i;
         }
+        else if(!s[j].match(/[a-z0-9]/i)){
+            --j;
+        }
+       
+        else if(s[i] === s[j]){
+             ++i;
+             --j; 
+        }
+        else{
+            return false;
+        }
+       
     }
-    return filteredString === reversedString;
+    return true;
 };
