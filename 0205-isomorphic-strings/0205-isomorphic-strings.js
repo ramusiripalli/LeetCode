b926bce41f91cde0b776b59c2805a973dbe4eb19
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-11 12:46:06 +0530

    Time: 8 ms (32.72%), Space: 55 MB (53.33%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -4,22 +4,19 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-      const charIndexS = {};
-    const charIndexT = {};
-
-    for (let i = 0; i < s.length; i++) {
-        if (!(s[i] in charIndexS)) {
-            charIndexS[s[i]] = i;
+    let mapStoT = {};
+    let mapTtoS = {};
+    for(let i=0;i<s.length;i++){
+        if(!mapStoT[s[i]] && !mapTtoS[t[i]]){
+            mapStoT[s[i]] = t[i];
+            mapTtoS[t[i]] = s[i];
         }
-
-        if (!(t[i] in charIndexT)) {
-            charIndexT[t[i]] = i;
-        }
-
-        if (charIndexS[s[i]] !== charIndexT[t[i]]) {
+        else if(mapTtoS[t[i]] !== s[i]){
             return false;
         }
+        else if(mapStoT[s[i]] !== t[i]){
+            return true;
+        }
     }
-
-    return true;  
+    return true;
 };
