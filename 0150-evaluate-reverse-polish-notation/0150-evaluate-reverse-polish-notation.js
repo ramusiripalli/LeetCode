e29a8d7c29c4776badf0e3f0bc2b738e9f993fca
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-16 16:45:57 +0530

    Time: 5 ms (78.96%), Space: 56.6 MB (78.35%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -3,20 +3,25 @@
  * @return {number}
  */
 var evalRPN = function(tokens) {
- let stack = [];
- for(let i=0;i<tokens.length;i++){
+    let stack = [];
 
-    if(tokens[i] === '+' || tokens[i] === '-' || tokens[i] === '*' || tokens[i] === '/')
-    {
-        let right = stack.pop();
-        let left = stack.pop();
-        let ans = eval(`${left} ${tokens[i]} ${right}`);
-        stack.push(Math.trunc(ans));
-    }
-    else{
-        stack.push(tokens[i]);
+    let map = {
+        '+' : (left,right) => (left+right),
+        '-' : (left,right) => (left-right),
+        '*' : (left,right) => (left * right),
+        '/' : (left,right) => Math.trunc(left / right)
     }
 
- }
- return Number(stack.pop())
+    for(let i=0;i<tokens.length;i++){
+        if(map[tokens[i]]){
+            let right = stack.pop();
+            let left = stack.pop();
+            let ans = map[tokens[i]](Number(left),Number(right));
+            stack.push(ans);
+        }
+        else{
+            stack.push(tokens[i]);
+        }
+    }
+    return Number(stack.pop())
 };
