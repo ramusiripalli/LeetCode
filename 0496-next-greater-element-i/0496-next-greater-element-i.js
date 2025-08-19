0191b193fdaf343cb0662eb15389dd6dd80ee86b
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-19 18:03:48 +0530

    Time: 2 ms (87.73%), Space: 57.9 MB (36.46%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -3,19 +3,32 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var nextGreaterElement = function(nums1, nums2) {
-    let res = [];
-    for(let i=0;i<nums1.length;i++){
-        let num = nums1[i];
-        let index = nums2.indexOf(num);
-        let nextGreaterElement = -1;
-        for(let j=index+1; j<nums2.length;j++){
-            if(nums2[j] > num){
-                nextGreaterElement = nums2[j];
-                break;
+var nextGreaterElement = function(nums1, arr) {
+    let nextGreaterMap = {};
+    let stack = [];
+    let n = arr.length;
+    stack.push(arr[n-1]);
+    nextGreaterMap[arr[n-1]] = -1;
+
+    for(let i=n-2;i>=0;i--){
+            while(stack.length){
+                if(stack[stack.length-1] < arr[i])
+                {
+                    stack.pop()
+                }
+                else{
+                    nextGreaterMap[arr[i]] = stack[stack.length-1];
+                    break;
+                }
             }
-        }
-        res.push(nextGreaterElement);
+            if(stack.length === 0){
+                nextGreaterMap[arr[i]] = -1;
+            }
+        
+        stack.push(arr[i]);
     }
-    return res;
+
+   return nums1.map(x => nextGreaterMap[x]);
+
+
 };
