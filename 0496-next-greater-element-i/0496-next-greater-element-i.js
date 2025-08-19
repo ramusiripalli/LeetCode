abcc74427b925ffea7d39274af850907c7d2676d
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-19 16:35:45 +0530

    Time: 5 ms (38.5%), Space: 54.5 MB (96.73%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -8,14 +8,14 @@ var nextGreaterElement = function(nums1, nums2) {
     for(let i=0;i<nums1.length;i++){
         let num = nums1[i];
         let index = nums2.indexOf(num);
-        let greatestElement = -1;
-        for(let j=index+1;j<nums2.length;j++){
-                if(nums2[j] > num){
-                    greatestElement = nums2[j];
-                    break;
-                }
+        let nextGreaterElement = -1;
+        for(let j=index+1; j<nums2.length;j++){
+            if(nums2[j] > num){
+                nextGreaterElement = nums2[j];
+                break;
+            }
         }
-        res.push(greatestElement);
+        res.push(nextGreaterElement);
     }
     return res;
 };
