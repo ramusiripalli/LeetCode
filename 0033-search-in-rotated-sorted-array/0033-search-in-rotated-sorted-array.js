c21ae7b7da5b809646b7cb69ea2a3cb955af213a
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-20 20:50:17 +0530

    Time: 0 ms (100%), Space: 54.4 MB (15.49%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,9 +4,28 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    for(let i=0;i<nums.length;i++){
-        if(nums[i] === target){
-            return i;
+    let left = 0;
+    let right = nums.length-1;
+    while(left <= right){
+        let mid = left + Math.floor((right-left)/2);
+        if(target === nums[mid]) {
+            return mid;
+        }
+        if(nums[left] <= nums[mid]){
+            if(target < nums[mid] && target >= nums[left]){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        else{
+            if((target > nums[mid]) && (target <= nums[right])){
+                left = mid + 1;
+            }
+            else{
+                right = mid-1;
+            }
         }
     }
     return -1;
