5ac5d5126975283ee6c6cddfd76d24d05499d856
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-22 17:28:11 +0530

    Time: 1 ms (72.43%), Space: 55 MB (93.93%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -3,10 +3,21 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    let i = 0;
-    for( i ; i*i <= x;i++){
-       
-    }
-    return Math.floor(i-1);
+    if(x<2) return x;
+    let left = 2;
+    let right = Math.floor(x/2);
+    while(left <=right){
+        let mid = left + Math.floor((right-left)/2);
 
+        if(x === mid * mid){
+            return mid;
+        }
+        else if(x > mid*mid){
+            left = mid+1;
+        }
+        else{
+            right = mid - 1;
+        }
+    }
+    return right;
 };
