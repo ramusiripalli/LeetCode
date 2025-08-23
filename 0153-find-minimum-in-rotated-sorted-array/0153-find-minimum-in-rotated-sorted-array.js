ac3ca427a25d40ce9de8b379f86d3c794eca90d5
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-23 17:48:45 +0530

    Time: 0 ms (100%), Space: 55.4 MB (5.09%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -2,24 +2,12 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMin = function(a) {
-    let l = 0;
-    let r = a.length - 1;
-
-    while (l <= r) {
-        if (a[l] <= a[r]) {
-            return a[l];
-        }
-
-        let m = l + Math.floor((r - l) / 2);
-        if (a[m] < a[m - 1]) {
-            return a[m];
-        }
-
-        if (a[l] > a[m]) {
-            r = m - 1;
-        } else {
-            l = m + 1;
+var findMin = function(nums) {
+    let min = nums[0];
+    for(let i=1;i<nums.length;i++){
+        if(nums[i] < min){
+            min = nums[i];
         }
     }
+    return min;
 };
