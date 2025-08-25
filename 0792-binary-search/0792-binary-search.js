6260e2d1b9602cc3abd8b8da71333f05bdb3dd49
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-25 20:58:08 +0530

    Time: 0 ms (100%), Space: 58.3 MB (29.15%) - LeetHub

--- 0792-binary-search/0792-binary-search.js
+++ 0792-binary-search/0792-binary-search.js
@@ -6,16 +6,15 @@
 var search = function(nums, target) {
     let left = 0;
     let right = nums.length-1;
-    while(left <=right){
+    while(left <= right){
         let mid = left + Math.floor((right-left)/2);
-
         if(target === nums[mid]){
             return mid;
         }
-        else if(target > nums[mid]){
-            left = mid + 1;
+        else if(nums[mid] < target){
+            left = mid+1;
         }
-        else {
+        else{
             right = mid - 1;
         }
     }
