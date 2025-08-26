ce0fa905b4a800c6e2d402eeed1e5bf1ca5cf436
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-26 15:29:29 +0530

    Time: 6 ms (4.07%), Space: 61.9 MB (5%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -3,14 +3,14 @@
  * @return {number}
  */
 var singleNonDuplicate = function(nums) {
-    const freq = new Map();
-    for(const x of nums){
-        freq.set(x,(freq.get(x)||0) +1);
+    let freq = {};
+    for(let num of nums){
+        freq[num] = (freq[num] || 0) + 1;
     }
-
-    for(const [key,value] of freq.entries()){
-        if(value === 1) return key;
+    for(let key in freq){
+        if(freq[key] === 1){
+            return Number(key);
+        }
     }
-
     return -1;
 };
