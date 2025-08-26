3572422a839ba985fbc08e43b79862b33a818ab9
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-26 09:23:12 +0530

    Time: 1 ms (20.95%), Space: 54.4 MB (59.46%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -8,18 +8,18 @@ var searchRange = function(nums, target) {
     if(nums.length === 0) {
         return res;
     }
+    let first = -1;
+    let second = -1;
     for(let i=0;i<nums.length;i++){
         if(nums[i] === target){
-            res[0] = i;
-            break;
-        }
-    }
-    for(let j=nums.length-1;j>=0;j--){
-        if(nums[j] === target){
-           res[1] = j;
-            break;
-        }
-    }
 
-    return res;
+            if(first === -1){
+                first = i;
+            }
+          
+                second = i;
+            
+        }
+    }
+    return [first,second];
 };
