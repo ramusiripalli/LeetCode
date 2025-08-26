d7b2d38137a9b5d0f005724e1fcc0c196ad0d339
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-26 12:43:21 +0530

    Time: 0 ms (100%), Space: 64.6 MB (35.42%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.js
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.js
@@ -3,9 +3,17 @@
  * @return {number}
  */
 var peakIndexInMountainArray = function(arr) {
-    for(let i =0;i<arr.length;i++){
-        if(arr[i] > arr[i+1]){
-            return i;
+    let left = 0;
+    let right = arr.length-1;
+
+    while(left < right){
+        let mid = left + Math.floor((right-left)/2);
+        if(arr[mid] < arr[mid+1]){
+            left = mid+1;
+        }
+        else{
+            right = mid;
         }
     }
+    return right;
 };
