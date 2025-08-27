df6f952388a5bd368e6951078bbb2da7704e112b
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-27 22:59:19 +0530

    Time: 5 ms (78.9%), Space: 63.9 MB (76.66%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,14 +3,12 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-   let xCopy = x;
-   if(x<0) return false;
-   let rev = 0;
-   while(x>0){
-    let lastDigit = x%10;
-    rev = rev*10 + lastDigit;
-    x = Math.floor(x/10);
-   }
-
-   return rev === xCopy;
+    let dup = x;
+    let rev = 0;
+    while(x>0){
+        let lastDigit = x %10;
+        rev = rev*10 + lastDigit;
+        x = Math.floor(x/10);
+    }
+    return rev === dup;
 };
