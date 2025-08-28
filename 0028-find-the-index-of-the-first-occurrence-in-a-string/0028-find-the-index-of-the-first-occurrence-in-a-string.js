f5ba755bba7f84036bba5d38fb13df7a18b25eac
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-28 16:19:24 +0530

    Time: 1 ms (19.29%), Space: 54.1 MB (38.03%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,5 +4,21 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle);
+    let haystackLength = haystack.length;
+    let needleLength = needle.length;
+
+    for(let i=0;i<=haystackLength - needleLength; i++ ){
+        let j = 0;
+        for(j=0;j<needleLength;j++){
+            if(haystack[i+j] !== needle[j]){
+                break;
+            }
+
+        }
+
+        if(j === needleLength){
+            return i;
+        }
+    }
+    return -1;
 };
