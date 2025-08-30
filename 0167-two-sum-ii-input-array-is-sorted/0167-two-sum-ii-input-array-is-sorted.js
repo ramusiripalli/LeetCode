fef21c293bae9724d1567d9ff4ce90d3a2a2f61e
ramu siripalli <ramsiripalli2772@gmail.com>
2025-08-30 08:24:53 +0530

    Time: 0 ms (100%), Space: 53.9 MB (98.41%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -6,13 +6,14 @@
 var twoSum = function(numbers, target) {
     let left = 0;
     let right = numbers.length-1;
-    while(left < right){
-        let sum = numbers[left] + numbers[right];
 
-        if(sum > target){
+    while(left < right){
+
+        let sum = numbers[left] + numbers[right];
+        if( sum > target ){
             --right;
         }
-        else if(sum < target){
+        else if (sum < target){
             ++left;
         }
         else{
