0a8baa08fd6024bfb9b9210d59db926d0c552904
ramu siripalli <ramsiripalli2772@gmail.com>
2025-09-03 11:32:25 +0530

    Time: 1 ms (72.81%), Space: 54.2 MB (87.31%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -5,12 +5,17 @@
  */
 var isSubsequence = function(s, t) {
     let i=0;
-    let j=0;
-    while(j < t.length){
+    let j = 0;
+    while(j<t.length){
+
         if(s[i] === t[j]){
             ++i;
+            ++j;
         }
-        ++j;
+        else if(s[i] !== t[j]){
+            ++j;
+        }
     }
-    return i === s.length;
+
+    return (i === s.length) ? true : false;
 };
