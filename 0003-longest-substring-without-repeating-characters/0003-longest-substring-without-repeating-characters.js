cfef01fc361d08f2973b311c0f04b1d2a7afd431
ramu siripalli <ramsiripalli2772@gmail.com>
2025-09-03 17:47:03 +0530

    Time: 2 ms (98.52%), Space: 55.2 MB (98.4%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,18 +3,21 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let i = j = 0;
-    const map = {};
+    let i=0;
+    let j = 0;
+    const map = new Map();
     let maxWS = 0;
-    for(j=0;j<s.length;j++){
-        if(map[s[j]] !== undefined && map[s[j]] >= i){
-           i = map[s[j]] + 1;
+    for(j=0;j<s.length; j++){
+        
+        if(map.has(s[j]) && map.get(s[j]) >= i){
+
+            i = map.get(s[j]) + 1;
         }
-        map[s[j]] = j;
-        currWS = j - i + 1;
-        maxWS = Math.max(maxWS, currWS);
+        map.set(s[j],j);
 
+        let currWS = j - i + 1;
+        maxWS = Math.max(currWS, maxWS);
+       
     }
-
-    return maxWS;
+     return maxWS;
 };
