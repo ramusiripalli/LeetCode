51712303319708371a392a9b185dbe5aba0e3639
ramu siripalli <ramsiripalli2772@gmail.com>
2025-09-10 12:40:36 +0530

    Time: 0 ms (100%), Space: 55 MB (10.15%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -12,10 +12,10 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-  if(!p && !q) return true;
-  if(!p || !q) return false;
+    if(p=== null && q === null) return true;
+    if(p=== null || q=== null) return false;
 
-  return p.val === q.val && 
-  isSameTree(p.left, q.left) &&
-  isSameTree(p.right, q.right)
+    return p.val === q.val &&
+           isSameTree(p.left,q.left) &&
+           isSameTree(p.right, q.right);
 };
