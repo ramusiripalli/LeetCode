6a10657355403945a66a239f52bdd919ca17074f
ramu siripalli <ramsiripalli2772@gmail.com>
2025-09-10 15:26:36 +0530

    Time: 3 ms (42.94%), Space: 58.8 MB (51.88%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    if(!root) return [];
+    let queue = [root];
+    let ans = [];
+    while(queue.length>0){
+        let levelArray = [];
+        let levelSize = queue.length;
+        for(let i=0;i<levelSize;i++){
+            let curr = queue.shift();
+            if(curr.left) queue.push(curr.left);
+            if(curr.right) queue.push(curr.right);
+            levelArray.push(curr.val);
+        }
+        ans.push(levelArray);
+    }
+    return ans;
+};
