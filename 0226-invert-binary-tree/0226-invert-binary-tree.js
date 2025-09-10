ed7e29e4ea4a4fc6312af87083add932cfd7259a
ramu siripalli <ramsiripalli2772@gmail.com>
2025-09-10 08:57:04 +0530

    Time: 0 ms (100%), Space: 54.9 MB (11.6%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -12,13 +12,12 @@
  */
 var invertTree = function(root) {
     if(!root) return root;
-
     let temp = root.left;
     root.left = root.right;
     root.right = temp;
 
     invertTree(root.left);
     invertTree(root.right);
-
+    
     return root;
 };
