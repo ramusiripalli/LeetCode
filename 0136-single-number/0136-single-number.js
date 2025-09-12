e283ccacf62bef7943cf5dee581b74763fcdb692
ramu siripalli <ramsiripalli2772@gmail.com>
2025-09-12 22:22:11 +0530

    Time: 0 ms (100%), Space: 57.8 MB (34.9%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -4,9 +4,8 @@
  */
 var singleNumber = function(nums) {
     let xor = 0;
-    for(let i =0;i<nums.length;i++){
-        xor = xor ^ nums[i];
+    for(let i=0;i<nums.length;i++){
+        xor ^= nums[i];
     }
-
     return xor;
 };
