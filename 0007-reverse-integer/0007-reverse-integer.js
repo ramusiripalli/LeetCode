eed297589c18198c7df0ebac569949d78cf95327
ramu siripalli <ramsiripalli2772@gmail.com>
2025-09-12 21:39:32 +0530

    Time: 46 ms (76.52%), Space: 55.8 MB (35.27%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -8,11 +8,11 @@ var reverse = function(x) {
     let rev = 0;
     while(x>0){
         let lastDigit = x%10;
-        rev = rev *10+ lastDigit;
+        rev = rev * 10 + lastDigit;
         x = Math.floor(x/10);
     }
-  let limit = 2**31;
-  if(rev < -limit || rev > limit-1) return 0;
+    let limit = Math.pow(2,31);
+    if(rev > limit || rev < -(limit-1)) return 0;
 
-    return (xCopy<0) ? -rev : rev;
+    return xCopy > 0 ? rev : -rev; 
 };
