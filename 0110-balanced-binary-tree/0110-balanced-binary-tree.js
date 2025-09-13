93e5a2cb043a50cc6bf7eb79d7c16787e81c6976
ramu siripalli <ramsiripalli2772@gmail.com>
2025-09-13 12:24:19 +0530

    Time: 2 ms (48.35%), Space: 60.5 MB (45.45%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -12,14 +12,17 @@
  */
 var isBalanced = function(root) {
     let ans = true;
-    let calculateHeight = (curr) => {
+    function calculateHeight(curr){
         if(!curr) return 0;
         let leftHeight = calculateHeight(curr.left);
         let rightHeight = calculateHeight(curr.right);
-        if(Math.abs(leftHeight - rightHeight) > 1){
-            ans = ans && false;
+
+        if(Math.abs(leftHeight-rightHeight) > 1){
+            ans = false;
         }
-        return 1 + Math.max(leftHeight, rightHeight);
+
+
+        return 1+ Math.max(leftHeight,rightHeight)
     }
     calculateHeight(root);
     return ans;
