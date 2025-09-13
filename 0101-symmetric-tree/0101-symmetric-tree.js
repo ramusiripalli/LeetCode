a8945d572641c804b0df57f1d04ca7121a63a1bc
ramu siripalli <ramsiripalli2772@gmail.com>
2025-09-13 12:10:31 +0530

    Time: 0 ms (100%), Space: 55.8 MB (93.54%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.js
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -12,14 +12,16 @@
  */
 var isSymmetric = function(root) {
     
-    function isMirror(left,right) {
+    function isMirror(left,right){
+
         if(!left && !right) return true;
+
         if(!left || !right) return false;
 
         return left.val === right.val &&
-            isMirror(left.left,right.right) &&
-            isMirror(left.right,right.left);
+        isMirror(left.left,right.right) &&
+        isMirror(left.right,right.left);
+
     }
-    return isMirror(root.left, root.right);
-    
+    return isMirror(root.left,root.right)
 };
