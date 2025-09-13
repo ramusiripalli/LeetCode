f6df1f73c227ec271a72832652cbe21ced212d79
ramu siripalli <ramsiripalli2772@gmail.com>
2025-09-13 16:24:04 +0530

    Time: 0 ms (100%), Space: 54.7 MB (42.11%) - LeetHub

--- 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.js
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.js
@@ -2,15 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-function gcdOfTwoNumbers(a, b) {
-    while (b !== 0) {
-        [a, b] = [b, a % b];
-    }
-    return a;
-}
 
+function gcdOfTwoNumbers(num1,num2){
+    while(num1 > 0 && num2 > 0){
+        if(num1>num2){
+            num1 = num1%num2;
+        }
+        else{
+            num2 = num2 % num1;
+        }
+    }
+    if(num1 === 0) return num2;
+    else return num1;
+}
 var findGCD = function(nums) {
-    let minNum = Math.min(...nums);
-    let maxNum = Math.max(...nums);
-    return gcdOfTwoNumbers(minNum, maxNum);
+let minNumber = Math.min(...nums);
+let maxNumber = Math.max(...nums);
+
+return gcdOfTwoNumbers(minNumber,maxNumber);
+    
 };
