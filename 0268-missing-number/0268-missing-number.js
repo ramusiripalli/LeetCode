f7001f42ea7e3184f5b34b88db96c987a7dcac4f
ramu siripalli <ramsiripalli2772@gmail.com>
2025-09-14 12:35:49 +0530

    Time: 21 ms (14.57%), Space: 57.9 MB (19.99%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let n = nums.length;
-    let totalSum = n * (n+1)/2;
-    let sum = 0;
-    for(let i=0;i<n;i++){
-        sum += nums[i];
-    } 
-    return totalSum - sum;
+    nums.sort((a,b) => a - b);
+    for(let i=0;i<nums.length;i++){
+        if(nums[i] !== i){
+            return i;
+        }
+    }
+    return nums.length;
 };
