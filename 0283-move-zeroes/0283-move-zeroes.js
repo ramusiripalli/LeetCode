8104ee81be0b246efbf0303ed54b54f05c7a7f5f
ramu siripalli <ramsiripalli2772@gmail.com>
2025-09-14 12:45:18 +0530

    Time: 0 ms (100%), Space: 60.4 MB (48.79%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,15 +3,15 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
+    let n = nums.length;
     let x = 0;
-    for(let i=0;i<nums.length;i++){
+    for(let i=0;i<n;i++){
         if(nums[i] !== 0){
             nums[x] = nums[i];
-            x++; 
+            x++;
         }
     }
-    for(let i=x;i<nums.length;i++){
+    for(let i = x;i<n;i++){
         nums[i] = 0;
     }
-    
 };
