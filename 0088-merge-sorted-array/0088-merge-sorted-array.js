7e5ff75030ba212a4a31f36b0f26af70f651c8ed
ramu siripalli <ramsiripalli2772@gmail.com>
2025-09-14 09:49:57 +0530

    Time: 0 ms (100%), Space: 54.8 MB (52.46%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,18 +6,31 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let p1 = m-1;
-    let p2 = n-1;
-    for(let i = m+n-1; i>=0; i--){
-
-        if(p2 < 0) break;
-        if(nums1[p1] > nums2[p2] && p1 >=0 ){
-            nums1[i] = nums1[p1];
-            p1--;
+    let nums1Copy = nums1.slice(0,m);
+    let left = 0;
+    let right = 0;
+    let k = 0;
+    while(left < m && right < n){
+        if(nums1Copy[left] < nums2[right]){
+            nums1[k] = nums1Copy[left];
+            left++;
         }
         else{
-            nums1[i] = nums2[p2];
-            p2--;
+            nums1[k] = nums2[right];
+            right++;
         }
+        k++;
     }
+    while(left < m){
+        nums1[k] = nums1Copy[left];
+        left++;
+        k++;
+    }
+    while(right < n){
+        nums1[k] = nums2[right];
+        right++;
+        k++;
+    }
+
+    return nums1;
 };
