0c2e1f1e83d5b86e09471b99f1ddd05c3812cf21
ramu siripalli <ramsiripalli2772@gmail.com>
2025-09-15 11:54:13 +0530

    Time: 0 ms (100%), Space: 55.5 MB (9.29%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.js
+++ 0782-jewels-and-stones/0782-jewels-and-stones.js
@@ -4,16 +4,11 @@
  * @return {number}
  */
 var numJewelsInStones = function(jewels, stones) {
-    let jewelSet = new Set();
-    for(let i=0;i<jewels.length;i++){
-        jewelSet.add(jewels[i]);
-    }
     let count = 0;
-    for(let i=0;i<stones.length;i++){
-       if(jewelSet.has(stones[i])){
-        count++;
-       }
+    for(let ch of stones){
+        if(jewels.includes(ch)){
+            count++;
+        }
     }
     return count;
-
 };
