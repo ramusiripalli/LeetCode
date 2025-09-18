e746f3399e669e4a8b21325080ae6c9b9d5d1794
ramu siripalli <ramsiripalli2772@gmail.com>
2025-09-18 10:04:10 +0530

    Time: 5 ms (63.58%), Space: 56.3 MB (64.25%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.js
+++ 0541-reverse-string-ii/0541-reverse-string-ii.js
@@ -5,15 +5,14 @@
  */
 var reverseStr = function(s, k) {
     s = s.split('');
-    for(let x = 0; x<s.length;x+=(2*k)){
+    for(let x=0;x<s.length;x+=(2*k)){
         let n = k;
         let mid = Math.floor(n/2);
-        for(let i = 0;i<mid;i++){
+        for(let i=0;i<mid;i++){
             let temp = s[x+i];
             s[x+i] = s[x+n-i-1];
             s[x+n-i-1] = temp;
         }
-
     }
     return s.join('');
 };
