5b306231d99e8a452b255062b1ea98c905259ecd
ramu siripalli <ramsiripalli2772@gmail.com>
2025-09-19 17:03:11 +0530

    Time: 2 ms (83.19%), Space: 66.1 MB (13.97%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,16 +3,15 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let n = prices.length;
+    let minPrice = prices[0];
     let maxProfit = 0;
-    let min = prices[0];
-    for(let i = 1;i<n;i++){
-        if(prices[i] < min){
-            min = prices[i];
+    for(let i=1;i<prices.length;i++){
+        if(prices[i] < minPrice){
+            minPrice = prices[i];
         }
 
-        if(prices[i] - min > maxProfit){
-            maxProfit = prices[i] - min;
+        if((prices[i] - minPrice) > maxProfit){
+            maxProfit = prices[i] - minPrice;
         }
     }
     return maxProfit;
