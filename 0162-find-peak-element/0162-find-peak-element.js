38fccc96ec64673e1695d5c4ef2d1718a538f661
ramu siripalli <ramsiripalli2772@gmail.com>
2025-09-23 14:20:03 +0530

    Time: 0 ms (100%), Space: 54.3 MB (20.94%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -3,16 +3,10 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    let left = 0;
-    let right = nums.length-1;
-    while(left < right){
-        let mid = Math.floor((left + right)/2);
-        if(nums[mid] < nums[mid+1]){
-            left = mid+1;
-        }
-        else{
-            right = mid;
+    for(let i=0;i<nums.length;i++){
+        if((i==0 || nums[i-1] < nums[i])  && ( i=== nums.length-1  || nums[i] > nums[i+1] )){
+            return i;
         }
     }
-    return left;
+    return -1;
 };
