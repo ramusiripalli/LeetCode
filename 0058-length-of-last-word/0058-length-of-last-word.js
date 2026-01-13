d792616a7a0fb13fb110b58bcaed4893e058cda3
ramu siripalli <ramsiripalli2772@gmail.com>
2026-01-13 06:59:51 +0530

    Time: 0 ms (100%), Space: 53.4 MB (85.58%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -4,16 +4,23 @@
  */
 var lengthOfLastWord = function(s) {
     let n = s.length-1;
-    let count = 0;
-    while(n>=0){
-
-        if(s[n] !== " "){
-            ++count;
-        }
-        else if(count>0){
+    while(n >= 0){
+        if(s[n] === " "){
+            --n;
+        }else{
             break;
         }
-        --n;
+    }
+    let count = 0;
+
+    while(n>=0){
+        if(s[n] !== " "){
+            ++count;
+            --n;
+        }
+        else{
+            break;
+        }
     }
     return count;
 };
