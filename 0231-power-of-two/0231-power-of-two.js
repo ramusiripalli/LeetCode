e6e13476ae26fd75b17cefe875a7be6420a31faa
ramu siripalli <ramsiripalli2772@gmail.com>
2026-01-30 14:10:49 +0530

    Time: 1 ms (31.24%), Space: 57.4 MB (18.49%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.js
+++ 0231-power-of-two/0231-power-of-two.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    if(n == 1) return true;
-    if(n < 1 || (n%2 !== 0)) return false;
+    if(n === 1) return true;
+    if(n<1 || n%2 !== 0) return false;
     return isPowerOfTwo(n/2);
 };
