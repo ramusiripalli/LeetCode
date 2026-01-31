df35e3892f07b0702b512e0fc7c7917151577183
ramu siripalli <ramsiripalli2772@gmail.com>
2026-01-31 16:41:04 +0530

    Time: 29 ms (72.15%), Space: 70.1 MB (10.47%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -4,14 +4,21 @@
  */
 var groupAnagrams = function(strs) {
     let map = new Map();
+  
     for(let word of strs){
-        let sortedWord = word.split('').sort().join('');
-
-        if(!map.has(sortedWord)){
-            map.set(sortedWord,[]);
+       let count = Array(26).fill(0);
+        for(let ch of word){
+            let index = ch.charCodeAt(0) - 97;
+            count[index]++
         }
-        map.get(sortedWord).push(word);
+        
+        let key = count.join("#");
+
+        if(!map.has(key)){
+            map.set(key,[]);
+        }
+        map.get(key).push(word);
     }
 
-    return [...map.values()];
+    return Array.from(map.values());
 };
