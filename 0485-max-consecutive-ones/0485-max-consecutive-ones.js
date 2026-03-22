33e41c2f5aa84c0b2d7fac483136dbcdc7b3b922
ramu siripalli <ramsiripalli2772@gmail.com>
2026-03-22 14:04:20 +0530

    Time: 1 ms (92.32%), Space: 60.1 MB (41.89%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.js
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -3,16 +3,19 @@
  * @return {number}
  */
 var findMaxConsecutiveOnes = function(nums) {
-    let maxOnes = 0;
-    let count = 0;
-    for(let i =0;i<nums.length;i++){
-        if(nums[i] == 1){
-            count++;
+   let count = 0;
+   let max_count = 0;
+   for(let i=0;i<nums.length;i++){
+    if(nums[i] === 1){
+        count = count + 1;
+        if(count > max_count){
+            max_count = count;
         }
-        else{
-            count = 0;
-        }
-        maxOnes = Math.max(count,maxOnes);
+    }   
+    else{
+        count = 0;
     }
-    return maxOnes;
+   } 
+
+   return max_count;
 };
