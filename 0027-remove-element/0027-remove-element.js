5ae4a38b18666fcb6918800ef6b372ca8c25432f
ramu siripalli <ramsiripalli2772@gmail.com>
2026-03-23 07:41:16 +0530

    Time: 0 ms (100%), Space: 54.5 MB (14.57%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -4,11 +4,12 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
+    let n =  nums.length;
     let x = 0;
-    for(let i=0;i<nums.length;i++){
-        if(nums[i] != val){
+    for(let i=0;i<n;i++ ){
+        if(nums[i] !== val){
             nums[x] = nums[i];
-            x = x+1;
+            x++;
         }
     }
     return x;
