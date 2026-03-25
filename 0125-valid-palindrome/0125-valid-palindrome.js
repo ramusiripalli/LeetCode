739d13e12bca492360b205f243ac88c029372af9
ramu siripalli <ramsiripalli2772@gmail.com>
2026-03-25 10:34:07 +0530

    Time: 9 ms (22.8%), Space: 61.4 MB (20.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,16 +4,16 @@
  */
 var isPalindrome = function(s) {
     s = s.toLowerCase();
-    let left = 0
+    let left = 0;
     let right = s.length-1;
     while(left < right){
         if(!s[left].match(/[a-z0-9]/i)){
             ++left;
         }
         else if(!s[right].match(/[a-z0-9]/i)){
-            --right;
+            right--;
         }
-        else if(s[left] == s[right]){
+        else if(s[left] === s[right]){
             ++left;
             --right;
         }
