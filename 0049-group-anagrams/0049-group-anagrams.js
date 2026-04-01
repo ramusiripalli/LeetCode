d0d3c5cce4735b5d91d9887ec98bb6a5bbee9f92
ramu siripalli <ramsiripalli2772@gmail.com>
2026-04-01 14:17:27 +0530

    Time: 18 ms (98.09%), Space: 68.6 MB (36.81%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,22 +3,14 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    let map = new Map();
-  
-    for(let word of strs){
-       let count = Array(26).fill(0);
-        for(let ch of word){
-            let index = ch.charCodeAt(0) - 97;
-            count[index]++
-        }
-        
-        let key = count.join("#");
+    const map = new Map();
 
+    for(let word of strs){
+        let key = word.split("").sort().join('');
         if(!map.has(key)){
             map.set(key,[]);
         }
         map.get(key).push(word);
     }
-
-    return Array.from(map.values());
+    return [...map.values()];
 };
