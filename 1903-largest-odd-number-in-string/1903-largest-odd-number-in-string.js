96897f5f914a827a1ba7b35e95c2ae00d790a9bd
ramu siripalli <ramsiripalli2772@gmail.com>
2026-06-10 10:01:29 +0530

    Time: 3 ms (26.55%), Space: 57.8 MB (87.74%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
@@ -3,11 +3,10 @@
  * @return {string}
  */
 var largestOddNumber = function(num) {
-  for(let i=num.length-1;i>=0;i--){
-    let digit = num[i];
-    if(digit%2 === 1){
-        return num.substring(0,i+1);
+    for(let i=num.length-1;i>=0;i--){
+        if(Number(num[i]) % 2 === 1){
+            return num.slice(0,i+1);
+        }
     }
-  }
-  return "";
+    return "";
 };
