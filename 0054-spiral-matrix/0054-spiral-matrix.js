5e42df9a6c86cb4f3fe7c445aebb7b2c2f4c5e59
ramu siripalli <ramsiripalli2772@gmail.com>
2026-07-01 19:31:17 +0530

    Time: 0 ms (100%), Space: 54 MB (21.33%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.js
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -3,49 +3,38 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
-    let ans = [];
-        
-        // Number of rows
-        let n = matrix.length;
-
-        // Number of columns
-        let m = matrix[0].length;
-        
-        // Initialize pointers for traversal
-        let top = 0, left = 0;
-        let bottom = n - 1, right = m - 1;
-        
-        // Traverse the matrix in spiral order
-        while (top <= bottom && left <= right) {
-            // Traverse from left to right
-            for (let i = left; i <= right; ++i) {
-                ans.push(matrix[top][i]);
-            }
-            top++;
-            
-            // Traverse from top to bottom
-            for (let i = top; i <= bottom; ++i) {
-                ans.push(matrix[i][right]);
-            }
-            right--;
-            
-            // Traverse from right to left
-            if (top <= bottom) {
-                for (let i = right; i >= left; --i) {
-                    ans.push(matrix[bottom][i]);
-                }
-                bottom--;
-            }
-            
-            // Traverse from bottom to top
-            if (left <= right) {
-                for (let i = bottom; i >= top; --i) {
-                    ans.push(matrix[i][left]);
-                }
-                left++;
-            }
+    let rows = matrix.length;
+    let columns = matrix[0].length;
+    let top=0;
+    let bottom = rows-1;
+    let left =0;
+    let right = columns-1;
+    let result =[];
+    while(left <= right && top <= bottom){
+        for(let row=left;row<=right;row++){
+            result.push(matrix[top][row]);
         }
-        
-        //Return the ans
-        return ans;
+        top++;
+
+        for(let col=top;col<=bottom;col++){
+            result.push(matrix[col][right]);
+        }
+        right--;
+
+        if(top <= bottom){
+            for(let row=right;row>=left;row--){
+                result.push(matrix[bottom][row]);
+            }
+            bottom--;
+        }
+
+        if(left <= right){
+            for(let col=bottom;col>=top;col--){
+                result.push(matrix[col][left]);
+            }
+            left++;
+        }
+    }
+
+    return result;
 };
