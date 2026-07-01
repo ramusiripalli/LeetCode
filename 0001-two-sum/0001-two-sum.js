9206e459802320a70ffedd3f914e3bcbb1dad358
ramu siripalli <ramsiripalli2772@gmail.com>
2026-07-01 18:59:02 +0530

    Time: 47 ms (14.88%), Space: 54.4 MB (74.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -7,11 +7,11 @@ var twoSum = function(nums, target) {
     let n = nums.length;
     for(let i=0;i<n-1;i++){
         for(let j=i+1;j<n;j++){
-            if((nums[i] + nums[j]) === target){
-                return [i,j];
-            }
+if(nums[i] + nums[j] === target){
+    return [i,j];
+}
         }
-       
+
     }
-     return [];
+    return -1;
 };
