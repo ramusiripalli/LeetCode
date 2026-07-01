b4e4c5b30c55bf9d2006b19fe42a149a7823dd1f
ramu siripalli <ramsiripalli2772@gmail.com>
2026-07-01 15:15:23 +0530

    Time: 1 ms (48.43%), Space: 56.6 MB (87.79%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -5,10 +5,10 @@
 var removeDuplicates = function(nums) {
     let uniqueIndex = 0;
     for(let i=1;i<nums.length;i++){
-         if(nums[i] !== nums[uniqueIndex]){
-            uniqueIndex++;
+        if(nums[i] > nums[uniqueIndex]){
+            uniqueIndex = uniqueIndex + 1;
             nums[uniqueIndex] = nums[i];
-         }
+        }
     }
-    return uniqueIndex + 1;
+    return uniqueIndex+1;
 };
