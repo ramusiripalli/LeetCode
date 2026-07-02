0bd23149870c6ffa23079f83ba7b4785cc8633a6
ramu siripalli <ramsiripalli2772@gmail.com>
2026-07-02 08:46:04 +0530

    Time: 11 ms (77.53%), Space: 54.4 MB (95.33%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,5 +4,19 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    return s.split('').sort().join('') === t.split('').sort().join('');
+    if(s.length !== t.length) return false;
+    const freqMap = new Map();
+
+    for(let i=0;i<s.length;i++){
+        freqMap.set(s[i],(freqMap.get(s[i]) || 0)+1 );
+    }
+
+    for(let j=0;j<t.length;j++){
+        if(!freqMap.has(t[j]) || freqMap.get(t[j]) <= 0){
+           return false;
+        }
+         freqMap.set(t[j],freqMap.get(t[j])-1);
+    }
+    return true;
+
 };
