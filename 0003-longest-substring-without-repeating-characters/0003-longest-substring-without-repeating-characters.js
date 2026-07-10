6a73914160bbbba6fb4e09c5d357d7cf02ce4609
ramu siripalli <ramsiripalli2772@gmail.com>
2026-07-10 14:23:29 +0530

    Time: 4 ms (87.33%), Space: 57 MB (80.43%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,22 +3,18 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    
-    const map = new Map();
-    let left = 0;
-    let maxLength = 0;
-    let currentWindowLength = 0;
+        const map = new Map();
+        let currWindow = 0;
+        let maxWindow=0;
+        let left = 0;
+        for(let right=0;right<s.length;right++){
+            if(map.has(s[right]) && map.get(s[right]) >= left){
+                left = map.get(s[right]) + 1;
+            }
 
-    for(let right=0;right<s.length;right++){
-        if(map.has(s[right]) && map.get(s[right]) >= left){
-            left = map.get(s[right]) + 1;
+            map.set(s[right],right);
+            currWindow = right-left+1;
+            maxWindow = Math.max(currWindow,maxWindow);
         }
-
-        map.set(s[right],right);
-        currentWindowLength = right-left+1;
-        maxLength = Math.max(maxLength,currentWindowLength);
-    }
-
-    return maxLength;
-
-};
+        return maxWindow;
+    };
