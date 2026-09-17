023073e55eb8af499d6a95857553e85492be6a90
ramu siripalli <ramsiripalli2772@gmail.com>
2026-09-17 11:48:13 +0530

    Time: 0 ms (100%), Space: 55 MB (9.66%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -6,22 +6,19 @@ var sortColors = function(nums) {
     let low = 0;
     let mid = 0;
     let high = nums.length-1;
-
-    while(mid<=high){
+    while(mid <= high){
         if(nums[mid] === 0){
-            let temp = nums[mid];
-            nums[mid] = nums[low];
-            nums[low] = temp;
+            [nums[low],nums[mid]] = [nums[mid],nums[low]];
             low++;
             mid++;
         }
         else if(nums[mid] === 1){
             mid++;
-        }
-        else{
+        }else{
             [nums[mid],nums[high]] = [nums[high],nums[mid]];
             high--;
         }
+
     }
     return nums;
 };
